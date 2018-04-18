d175cfef07d1ad1926540e6a71e78b0eb84e997f
Benjamin Bohak <benjamin.bohak@student.um.si>
2018-04-18 22:32:23 +0200

    Changed the whole game structure and added, the algorithm and also moved the class into a seperate file :)

---
+++ UI_Vaja_2/BoardState.cs
@@ -0,0 +1,122 @@
+﻿namespace UI_Vaja_2
+{
+    internal class BoardState
+    {
+        private int[,] board;
+        private int ocena;
+
+        public BoardState()
+        {
+            this.board = new int[3, 3];
+        }
+
+        public BoardState(int[,] a)
+        {
+            this.board = (int[,])a.Clone();
+            int ocenaMax = BoardHeuristics(a, 2);
+            int ocenaMin = BoardHeuristics(a, 1);
+            this.ocena = ocenaMax - ocenaMin;
+        }
+
+        public int BoardHeuristics(int[,]a, int player)
+        {
+            int taken_line = 0;
+
+            if (a[0, 0] == player || a[0, 1] == player || a[0, 2] == player)
+            {
+                taken_line++;
+            }
+            if (a[1, 0] == player || a[1, 1] == player || a[1, 2] == player)
+            {
+                taken_line++;
+            }
+            if (a[2, 0] == player || a[2, 1] == player || a[2, 2] == player)
+            {
+                taken_line++;
+            }
+            if (a[0, 0] == player || a[1, 0] == player || a[2, 0] == player)
+            {
+                taken_line++;
+            }
+            if (a[0, 1] == player || a[1, 1] == player || a[2, 1] == player)
+            {
+                taken_line++;
+            }
+            if (a[0, 2] == player || a[1, 2] == player || a[2, 2] == player)
+            {
+                taken_line++;
+            }
+            if (a[0, 2] == player || a[1, 1] == player || a[2, 0] == player)
+            {
+                taken_line++;
+            }
+            if (a[2, 0] == player || a[1, 1] == player || a[0, 2] == player)
+            {
+                taken_line++;
+            }
+
+            return (8 - taken_line);
+        }
+
+        public void setBoard(int[,] a)
+        {
+            this.board = (int[,])a.Clone();
+            int ocenaMax = BoardHeuristics(a, 2);
+            int ocenaMin = BoardHeuristics(a, 1);
+            this.ocena = ocenaMax - ocenaMin;
+        }
+
+        public void setBoard()
+        { 
+            this.board = new int[3, 3];
+        }
+
+        public int[, ]getBoard()
+        {
+            return this.board;
+        }
+
+        public void setBoardAtPosition(int x, int y, int value)
+        {
+            board[x, y] = value;
+            int ocenaMax = BoardHeuristics(this.board, 2);
+            int ocenaMin = BoardHeuristics(this.board, 1);
+            this.ocena = (ocenaMax - ocenaMin);
+        }
+
+        public int getBoardAtPosition(int x, int y)
+        {
+            return board[x, y];
+        }
+
+        public void setOcena(int ocena)
+        {
+            this.ocena = ocena;
+        }
+
+        /*
+        public int getOcena()
+        {
+            return ocena;
+        }*/
+
+        public int getOcena()
+        {
+            int ocenaMax = BoardHeuristics(this.board, 2);
+            int ocenaMin = BoardHeuristics(this.board, 1);
+            return (ocenaMax - ocenaMin);
+        }
+
+        public void Reset()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = 0;
+                }
+            }
+        }
+
+    }
+}
--- UI_Vaja_2/Form1.cs
+++ UI_Vaja_2/Form1.cs
@@ -1,37 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UI_Vaja_2
 {
-
-
     public partial class Form1 : Form
     {
-       public class BoardState
-        {
-            public int[,] board { get; set; }
-            public int heu { get; set; }
-            public int ocena { get; set; }
-            public BoardState next { get; set; }
-
-        }
-
-
-        int[,] Board = new int[3, 3];
+        private BoardState GameBoard = new BoardState();
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        void DISPLAY()
+        private void DISPLAY()
         {
             Graphics g1;
 
@@ -43,8 +26,8 @@ namespace UI_Vaja_2
 
             for (int i = 1; i < 3; i++)
             {
-                g1.DrawLine(gridPen, 215*i, 0, 215*i, 650);
-                g1.DrawLine(gridPen, 0, 215*i, 650, 215*i);
+                g1.DrawLine(gridPen, 215 * i, 0, 215 * i, 650);
+                g1.DrawLine(gridPen, 0, 215 * i, 650, 215 * i);
             }
             int fontSize = 146;
 
@@ -61,29 +44,25 @@ namespace UI_Vaja_2
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if(Board[i,j] == 1)
+                    if (GameBoard.getBoardAtPosition(i,j) == 1)
                     {
                         drawPoint = new PointF((0 + (i * 216)), (0 + (j * 216)));
                         g1.DrawString(drawX, drawFont, drawBrush, drawPoint);
                     }
-                    else if (Board[i, j] == 2)
+                    else if (GameBoard.getBoardAtPosition(i, j) == 2)
                     {
                         drawPoint = new PointF((0 + (i * 216)), (0 + (j * 216)));
                         g1.DrawString(drawO, drawFont, drawBrush, drawPoint);
                     }
                 }
-
             }
-
-
         }
 
-        int STATE(int[,]a)
+        private int STATE(int[,] a)
         {
-
             int j = 0;
 
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 if (a[i, j] != 0)
                 {
@@ -98,15 +77,13 @@ namespace UI_Vaja_2
                     {
                         return 1;
                     }
-                    if(i == 2)
+                    if (i == 2)
                     {
                         if (a[i, 0] == 1 && a[i - 1, 1] == 1 && a[i - 2, 2] == 1)
                         {
                             return 1;
                         }
                     }
-
-
 
                     if (i == 0)
                     {
@@ -126,11 +103,10 @@ namespace UI_Vaja_2
                             return 2;
                         }
                     }
-
                 }
             }
             j = 0;
-            for(int k = 0; k < 3; k++)
+            for (int k = 0; k < 3; k++)
             {
                 if (a[0, k] == 1 && a[1, k] == 1 && a[2, k] == 1)
                 {
@@ -140,131 +116,123 @@ namespace UI_Vaja_2
                 {
                     return 2;
                 }
-
             }
 
-            return 0; 
+            return 0;
         }
 
-        int HEURISTICS_PLAYER(int[,] a, int player) //checks all 8 posible good outcomes if they are taken 
+        private List<BoardState> MOVES(BoardState P, int player)
         {
-            int taken_line = 0;
+            List<BoardState> boardStates = new List<BoardState>();
 
-            if(a[0, 0] == player || a[0, 1] == player || a[0, 2] == player)
+            for (int i = 0; i < 3; i++)
             {
-                taken_line++;
-            }
-            if (a[1, 0] == player || a[1, 1] == player || a[1, 2] == player)
-            {
-                taken_line++;
-            }
-            if (a[2, 0] == player || a[2, 1] == player || a[2, 2] == player)
-            {
-                taken_line++;
-            }
-            if (a[0, 0] == player || a[1, 0] == player || a[2, 0] == player)
-            {
-                taken_line++;
-            }
-            if (a[0, 1] == player || a[1, 1] == player || a[2, 1] == player)
-            {
-                taken_line++;
-            }
-            if (a[0, 2] == player || a[1, 2] == player || a[2, 2] == player)
-            {
-                taken_line++;
-            }
-            if (a[0, 2] == player || a[1, 1] == player || a[2, 0] == player)
-            {
-                taken_line++;
-            }
-            if (a[2, 0] == player || a[1, 1] == player || a[0, 2] == player)
-            {
-                taken_line++;
-            }
-
-            return (8-taken_line); 
-        }
-
-        int HEURISTICS(int[,] a)
-        {
-            return (HEURISTICS_PLAYER(a, 2) - HEURISTICS_PLAYER(Board, 1));
-        }
-        
-        int DYNAMIC_GRADE(BoardState P, int player)
-        {
-            while(P.next != null)
-            {
-                if (player == 1)
+                for (int j = 0; j < 3; j++)
                 {
-                    if (P.ocena < P.next.ocena)
+                    if (P.getBoardAtPosition(i,j) == 0)
                     {
-                        P.ocena = P.next.ocena;
+                        BoardState newState = new BoardState();
+                        newState.setBoard(P.getBoard());
+                        newState.setBoardAtPosition(i,j,player);
+                        boardStates.Add(newState);
                     }
-                    P = P.next;
-                }
-                else if(player == 2)
-                {
-                    if (P.ocena > P.next.ocena)
-                    {
-                        P.ocena = P.next.ocena;
-                    }
-                    P = P.next;
-
                 }
             }
 
-            return P.ocena;
+            /*
+            Console.WriteLine("\nFIRST BOARD (INITIAL): ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (P.board[i, j] > -1)
+                    {
+                        Console.Write(" " + P.board[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("" + P.board[i, j]);
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("\n");
+
+            foreach(var BS in boardStates)
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (BS.board[i, j] > -1)
+                        {
+                            Console.Write(" " + BS.board[i, j]);
+                        }
+                        else
+                        {
+                            Console.Write("" + BS.board[i, j]);
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+                Console.WriteLine("");
+            }
+            */
+
+            return boardStates;
         }
 
-
-
-        BoardState OMEJENI_MINIMAKS(BoardState P, int player, int d)
+        private BoardState OMEJENI_MINIMAKS(BoardState P, int player, int d)
         {
-            if(P.next == null || d == 0)
+            if (STATE(P.getBoard()) != 0 || d == 0)
             {
-                P.heu = HEURISTICS(P.board);
                 return P;
             }
-            if(player == 1)
+            if (player == 1)
             {
-                P.ocena = -100000;
+                P.setOcena(-100000);
             }
-            else if(player == 2)
+            else if (player == 2)
             {
-                P.ocena = 100000;
+                P.setOcena(100000);
             }
-            
 
+            P.setBoard();
 
+            List<BoardState> M = new List<BoardState>();
+            M = MOVES(P, player);
+
+            BoardState R = new BoardState();
+
+            foreach (var m in M)
+            {
+                P.setBoard(m.getBoard());
+
+                if (player == 1)
+                {
+                    R = OMEJENI_MINIMAKS(R, player + 1, (d - 1));
+                }
+                else if (player == 2)
+                {
+                    R = OMEJENI_MINIMAKS(R, player - 1, (d - 1));
+                }
+                else
+                {
+                    Console.WriteLine("\n\n\n\n FATAL ERROR ?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?! \n\n\n\n");
+                }
+
+                if ((player == 1 && R.getOcena() > P.getOcena()) || (player == 2 && R.getOcena() < P.getOcena()))
+                {
+                    P.setBoard(R.getBoard());
+                }
+            }
             return P;
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-            for(int i = 0; i < 3; i++)
-            {
-                for(int j = 0; j < 3; j++)
-                {
-                    /*
-                    if(((i+j)%2) == 0)
-                    {
-                        Board[i, j] = 1;
-                    }
-                    else
-                    {
-                        Board[i, j] = 2;
-                    }*/
-                    Board[i, j] = 0;
-                }
-            }
-
-            Board[1, 0] = 2;
-
+            GameBoard.Reset();
             DISPLAY();
-
         }
 
         private void PlayBtn_Click(object sender, EventArgs e)
@@ -272,70 +240,53 @@ namespace UI_Vaja_2
             int level = Int32.Parse(LevelBox.Text.ToString());
 
             Console.WriteLine("Level je: " + level);
-
-
         }
 
-        void CLEAR()
+        private void CLEAR()
         {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Board[i, j] = 0;
-                }
-            }
-            
-
+            GameBoard.Reset();
             DISPLAY();
-            
         }
 
         private void GameBox_Click(object sender, EventArgs e)
         {
+            int level = Int32.Parse(LevelBox.Text.ToString());
+
             MouseEventArgs MC = (MouseEventArgs)e;
             Point coordinates = MC.Location;
 
             int x, y;
-            x = coordinates.X-8;
-            y = coordinates.Y-8;
+            x = coordinates.X - 8;
+            y = coordinates.Y - 8;
 
             Xcoord.Text = x.ToString();
             Ycoord.Text = y.ToString();
             x = (x / 210);
             y = (y / 210);
-            /*
-            if(Board[x, y] == 1 )
-                Board[x, y] = 2;
-            else if (Board[x, y] == 2)
-                Board[x, y] = 1;
-            */
-            if(Board[x, y] == 0)
+            
+            DISPLAY();
+
+            if (GameBoard.getBoardAtPosition(x,y) == 0)
             {
-                Board[x, y] = 1;
+                GameBoard.setBoardAtPosition(x, y, 1);
             }
             else
             {
-                WarningLbl.Text = "Field x: " + x + " y: " + y + " already taken!!!" ;
+                WarningLbl.Text = "Field x: " + x + " y: " + y + " already taken!!!";
             }
-
-
+            
+            GameBoard = OMEJENI_MINIMAKS(GameBoard, 2, level);
+            
             DISPLAY();
 
-            int a = STATE(Board);
-
-            int heuP = HEURISTICS(Board);
-
-            WarningLbl.Text = "h(P): " + heuP;
-            
+            int a = STATE(GameBoard.getBoard()); 
 
             if (a != 0)
             {
-                if(a == 1)
+                if (a == 1)
                 {
                     WarningLbl.Text = "Zmagal je igralec X";
                     CLEAR();
-
                 }
                 if (a == 2)
                 {
@@ -343,8 +294,6 @@ namespace UI_Vaja_2
                     CLEAR();
                 }
             }
-            
-
         }
 
         private void ResetBtn_Click(object sender, EventArgs e)
